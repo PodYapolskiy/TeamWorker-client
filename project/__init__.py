13884c4fd8ba350b17cbc9b64149fa1f1360a386
Anatoly <podyapolskiyaa@gmail.com>
2021-06-07 16:01:37 +0300

    Change proccess of downloading tasks; add 'change_task_state' function; add 'task_id' into every task_dict in tasks

--- project/__init__.py
+++ project/__init__.py
@@ -3,5 +3,3 @@ Project пакет
 В модуле `app` находятся функции, отвечающие за запросы к серверу.
 В модуле `functions` находятся вспомогательные функции.
 """
-from .app import sign, log, push_tasks_info, get_tasks_info, get_team_name, get_team_users
-from .functions import generate_string
